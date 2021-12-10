b4c1e019cb862d330bf10cc45515e776b12c1e85
Simon Busch <simon.busch@live.fr>
2021-12-10 11:40:23 +0100

    add logic to win and loss count + attack function logic

--- contracts/ZombieAttack.sol
+++ contracts/ZombieAttack.sol
@@ -13,7 +13,20 @@ contract ZombieAttack is ZombieHelper {
     return uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % _modulus;
   }
 
-  function attack(uint _zombieId, uint _targetId) external {
-
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    // get a random number for the outcome of the battle
+    uint rand = randMod(100);
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    } else {
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(myZombie);
+    }
   }
 }
