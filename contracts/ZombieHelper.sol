f0c39657965302de90c580fce8ba76b61444560e
Simon Busch <simon.busch@live.fr>
2021-12-10 06:32:16 +0100

    add level up function and f ees

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -4,7 +4,9 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // Start here
+  //fee to level up
+  uint levelUpFee = 0.001 ether;
+
   modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
@@ -15,8 +17,27 @@ contract ZombieHelper is ZombieFeeding {
     zombies[_zombieId].name = _newName;
   }
 
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
+  }
+
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
+  }
+
+  // return an array of zombies owned
+  function getZombiesByOwner(address _owner) external view returns (uint[] memory) {
+    // will store the number of zombies in an array
+    uint[] memory result = new uint[](ownerZombieCount[_owner]);
+    uint counter = 0;
+    for (uint i = 0; i < zombies.length; i++) {
+      if (zombieToOwner[i] == _owner) {
+        result[counter] = i;
+        counter++;
+      }
+    }
+    return result;
   }
 }
