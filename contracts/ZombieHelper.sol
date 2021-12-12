ae213f374d5d3e920276ed881f4fa07c795eedb0
Simon Busch <simon.busch@live.fr>
2021-12-12 08:17:26 +0100

    small fix and deploy script

--- contracts/ZombieHelper.sol
+++ contracts/ZombieHelper.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "./zombiefeeding.sol";
+import "./ZombieFeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,18 @@
+const hre = require("hardhat");
+
 const main = async () => {
+  console.log("hello")
   const [deployer] = await hre.ethers.getSigners();
   const accountBalance = await deployer.getBalance();
 
   console.log('Deploying contracts with account: ', deployer.address);
   console.log('Account balance: ', accountBalance.toString());
 
-  const Token = await hre.ethers.getContractFactory('ZombieFeeding');
-  const portal = await Token.deploy();
-  await portal.deployed();
+  const ZombieFactoryContract = await hre.ethers.getContractFactory('ZombieFactory');
+  const ZombieContract = await ZombieFactoryContract.deploy();
+  await ZombieContract.deployed();
 
-  console.log('Contract address: ', portal.address);
+  console.log("Contract deployed to:", ZombieContract.address);
 };
 
 const runMain = async () => {
