4bb722d9f22d7e0f6b391ac4a1ea9118e1ed4893
Simon Busch <simon.busch@live.fr>
2021-12-13 15:18:19 +0100

    fix visiblity and deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,26 @@ const main = async () => {
   const ZombieFactoryContract = await ZombieFactory.deploy();
   await ZombieFactoryContract.deployed();
   console.log("Contract ZombieFactoryContract deployed to:", ZombieFactoryContract.address);
+
+  const ZombieOwnership = await hre.ethers.getContractFactory("ZombieOwnership");
+  const ZombieOwnershipContract = await ZombieOwnership.deploy();
+  await ZombieOwnershipContract.deployed();
+  console.log("Contract ZombieOwnershipContract deployed to:", ZombieOwnershipContract.address);
+
+  const ZombieHelper = await hre.ethers.getContractFactory("ZombieHelper");
+  const ZombieHelperContract = await ZombieHelper.deploy();
+  await ZombieHelperContract.deployed();
+  console.log("Contract ZombieHelperContract deployed to:", ZombieHelperContract.address);
+
+  const ZombieFeeding = await hre.ethers.getContractFactory("ZombieFeeding");
+  const ZombieFeedingContract = await ZombieFeeding.deploy();
+  await ZombieFeedingContract.deployed();
+  console.log("Contract ZombieFeedingContract deployed to:", ZombieFeedingContract.address);
+
+  const ZombieAttack = await hre.ethers.getContractFactory("ZombieAttack");
+  const ZombieAttackContract = await ZombieAttack.deploy();
+  await ZombieAttackContract.deployed();
+  console.log("Contract ZombieAttackContract deployed to:", ZombieAttackContract.address);
 };
 
 const runMain = async () => {
